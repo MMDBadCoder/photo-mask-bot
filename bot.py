5678925206a11bb99dc9c037abdc4a41e129f167
Ali Hatami <hatam008@users.noreply.github.com>
2020-08-05 19:04:59 +0430

    comments added

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from PIL import Image
 from io import BytesIO
 from functools import wraps
 
+# This function will be used as a decorator for calback functions
 def send_action(action):
     def decorator(func):
         @wraps(func)
@@ -14,46 +15,61 @@ def send_action(action):
     
     return decorator
 
+# sending photo >>> action in bot
 send_uploading_photo = send_action(ChatAction.UPLOAD_PHOTO)
+# typing... action in bot
 send_typing = send_action(ChatAction.TYPING)
 
+# mask image load into memory here
 mask = Image.open("mask.png")
 mask_width, mask_height = mask.size
+
+# mask size in a Touple for PIL.Image.resize() methode
 mask_size = (mask_width,mask_height)
 
+# gets upadate and context from calback and photo_file (`cls`:telegram.File)
+# ceate PIL.Image from photo_file and add mask to it and send it back to user
 def process_image(update, context, photo_file):
     photo_byte = photo_file.download_as_bytearray()
     image = image_from_bytearray(photo_byte)
     masked_image = add_mask_to_background(image)
     send_image_to_user(context.bot,update.effective_chat.id,masked_image)
 
+# function for MessageHandler (image_handler) in main()
 def from_profile(update, context):
     profile_photos = context.bot.get_user_profile_photos(update.message.from_user.id)
-    if profile_photos.total_count == 0:
+    if profile_photos.total_count == 0: # if user doesn't have profile photo
         no_profile_error(update, context)
     else:
         send_from_profile(update,context,profile_photos)
 
+# send result from profile photo
 @send_uploading_photo
 def send_from_profile(update, context, profile_photos):
+    # photo_file will be last profile of user
     photo_file = profile_photos.photos[0][-1].get_file()
     process_image(update,context,photo_file)
 
+# send message that user doesn't have a profile poto
 @send_typing
 def no_profile_error(update, context):
     reply_text = "شما تصویر پروفایلی ندارید"
     context.bot.send_message(chat_id=update.effective_chat.id, text = reply_text)
 
+# send result from received photo
 @send_uploading_photo
 def from_send(update, context):
     photo_file = update.message.photo[-1].get_file()
     process_image(update,context,photo_file)
     
+# create PIL.Image from byte array
 def image_from_bytearray(byte_array):
     image = Image.open(BytesIO(byte_array))
     return image
 
-def crop_if_needed(image):
+# crop the image if it's not square and resize it to mask size
+# retures croped and resized image
+def crop_if_needed_and_resize(image):
     width, height = image.size
     if width == height:
         pass
@@ -66,11 +82,13 @@ def crop_if_needed(image):
     
     return image.resize(mask_size)
 
+# pasetes mask to the background and returns the result
 def add_mask_to_background(background):
-    background = crop_if_needed(background)
+    background = crop_if_needed_and_resize(background)
     background.paste(mask,(0,0),mask)
     return background
 
+# send PIL.Image to the user
 def send_image_to_user(bot, chat_id, image):
     bio = BytesIO()
     bio.name = 'masked.jpeg'
@@ -78,6 +96,7 @@ def send_image_to_user(bot, chat_id, image):
     bio.seek(0)
     bot.send_photo(chat_id, photo=bio)
 
+# function responsible for /start command
 @send_typing
 def start(update, context):
     keyboard = [["استفاده از عکس پروفایل"]]
@@ -85,10 +104,12 @@ def start(update, context):
     reply_text = "سلام\n" + "یک عکس برام بفرست یا با دکمه پایین عکس پروفایلت رو تغییر بده" + "\n" + "اگر به اطلاهات بیشتری نیاز داری  از /help استفاده کن"
     context.bot.send_message(chat_id=update.effective_chat.id, text = reply_text, reply_markup=reply_markup)
 
+# function responsible for unknown commands and messages
 @send_typing
 def unknown_format(update, context):
     update.message.reply_text("چنین دستوری برای من تعریف نشده است.")
 
+# function responsible for /help command
 @send_typing
 def help(update, context):
     reply_text = "شما میتونی یک عکس برای من بفرستی یا حتی از عکس فعلی پروفایلت استفاده کنی" + "\n" +  "برای استفاده از عکس پروفایل از دکمه \"استفاده از عکس پروفایل\" استفاده کن ." + "\n"
@@ -96,8 +117,10 @@ def help(update, context):
     reply_text3 = "بهتره هر قسمت از عکس رو میخوای قبل از فرستادن به صورت مربع برش بزنی. اگر عکس ارسالی مربع نباشه به صورت خودکار بزرگترین مربع وسطش برش میخوره."
     context.bot.send_message(chat_id=update.effective_chat.id, text = reply_text + reply_text2 + reply_text3)
 
+# main function
 def main():
-    token = "1199859151:AAG7JtLndlfvCDO_BJgHNsrYsMzWTLYkrzM"
+    # Replace "your_token" with <Your Token>
+    token = "your_token"
     updater = Updater(token= token,use_context= True)
     dispacher = updater.dispatcher
 
